4a0d4aac1ee94da7940c140e0095a935fca792aa
ahmed nader <ahmednader5012@gmail.com>
2025-11-18 18:54:26 +0200

    v1.7

--- components/sections/ServicesSection.tsx
+++ components/sections/ServicesSection.tsx
@@ -33,9 +33,6 @@ export default function ServicesSection() {
               Scope of Services
             </h2>
             <div className="h-1 w-24 bg-black mx-auto mb-6" />
-            <h3 className="text-2xl md:text-3xl font-bold text-black">
-              Hawai GAR for contracting
-            </h3>
           </div>
 
           {/* Civil Works Section - Priority 1 */}
@@ -212,7 +209,7 @@ export default function ServicesSection() {
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/50 to-black/30" />
                 <div className="absolute bottom-0 left-0 right-0 p-6">
-                  <h4 className="text-xl font-bold text-white">Plumbing</h4>
+                  <h4 className="text-xl font-bold text-white">Material Supplies</h4>
                 </div>
               </div>
             </div>
